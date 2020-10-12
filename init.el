634a0fea4d33965ce197544ea7ab3ea53a5462e7
Bing Guo <changethespace@gmail.com>
2020-10-12 19:30:27 -0400

    add mouse-start-end

--- init.el
+++ init.el
@@ -63,39 +63,6 @@
 (global-set-key (kbd "C-/") 'comment-region)
 
 
-(defun get-word-boundary ()
-  "Return the boundary of the current word.
-         The return value is of the form: (cons pos1 pos2).
-         "
-  (save-excursion
-    (let (p1 p2)
-      (progn
-        (skip-chars-backward "-A-Za-z0-9_.~/+*") ;; here you can choose which symbols to use
-        (setq p1 (point))
-        (skip-chars-forward "-A-Za-z0-9_.~/+*") ;; put the same here
-        (setq p2 (point)))
-      (cons p1 p2)
-      ))
-  )
-(defun select-word ()
-  "Mark the url under cursor."
-  (interactive)
-					;  (require 'thingatpt)
-  (let (bds)
-    (setq bds (get-word-boundary))
-    
-    (set-mark (car bds))
-    (goto-char (cdr bds))
-    )
-  )
-(global-set-key [double-mouse-1] 'select-word)
-
-
-;;;;;when double clicking with mouse, normally i want to select a long word which contain underscore and dot, to do this, the definition of word has to be changed.
-;(modify-syntax-entry ?_ "w" (standard-syntax-table))
-;(modify-syntax-entry ?. "w" (standard-syntax-table))
-;(modify-syntax-entry ?/ "w" (standard-syntax-table))
-
 (add-hook 'prog-mode-hook 'linum-mode)
 
 (global-set-key (kbd "C-x r") 'find-file-read-only)
@@ -845,3 +812,86 @@ Version 2019-01-16"
 
 (message "end----")
 (anarcatdisplay-timing)
+
+(defun mouse-start-end (start end mode)
+  "Return a list of region bounds based on START and END according to MODE.
+If MODE is 0 then set point to (min START END), mark to (max START END).
+If MODE is 1 then set point to start of word at (min START END),
+mark to end of word at (max START END).
+If MODE is 2 then do the same for lines."
+  (if (> start end)
+      (let ((temp start))
+        (setq start end
+              end temp)))
+  (setq mode (mod mode 3))
+  (cond ((= mode 0)
+	 (list start end))
+        ((and (= mode 1)
+              (= start end)
+	      (char-after start)
+              (= (char-syntax (char-after start)) ?\())
+         (if (/= (syntax-class (syntax-after start)) 4) ; raw syntax code for ?\(
+             ;; This happens in CC Mode when unbalanced parens in CPP
+             ;; constructs are given punctuation syntax with
+             ;; syntax-table text properties.  (2016-02-21).
+             (signal 'scan-error (list "Containing expression ends prematurely"
+                                       start start))
+           (list start
+                 (save-excursion
+                   (goto-char start)
+                   (forward-sexp 1)
+                   (point)))))
+        ((and (= mode 1)
+              (= start end)
+	      (char-after start)
+              (= (char-syntax (char-after start)) ?\)))
+         (if (/= (syntax-class (syntax-after start)) 5) ; raw syntax code for ?\)
+             ;; See above comment about CC Mode.
+             (signal 'scan-error (list "Unbalanced parentheses" start start))
+           (list (save-excursion
+                   (goto-char (1+ start))
+                   (backward-sexp 1)
+                   (point))
+                 (1+ start))))
+	((and (= mode 1)
+              (= start end)
+	      (char-after start)
+              (= (char-syntax (char-after start)) ?\"))
+	 (let ((open (or (eq start (point-min))
+			 (save-excursion
+			   (goto-char (- start 1))
+			   (looking-at "\\s(\\|\\s \\|\\s>")))))
+	   (if open
+	       (list start
+		     (save-excursion
+		       (condition-case nil
+			   (progn
+			     (goto-char start)
+			     (forward-sexp 1)
+			     (point))
+			 (error end))))
+	     (list (save-excursion
+		     (condition-case nil
+			 (progn
+			   (goto-char (1+ start))
+			   (backward-sexp 1)
+			   (point))
+		       (error end)))
+		   (1+ start)))))
+        ((= mode 1)
+	 (list (save-excursion
+		 (goto-char start)
+		 (skip-chars-backward "-A-Za-z0-9_.~/+*")
+		 (point))
+	       (save-excursion
+		 (goto-char end)
+		 (skip-chars-forward "-A-Za-z0-9_.~/+*") 
+		 (point))))
+        ((= mode 2)
+	 (list (save-excursion
+		 (goto-char start)
+		 (line-beginning-position 1))
+	       (save-excursion
+		 (goto-char end)
+		 (forward-line 1)
+		 (point))))))
