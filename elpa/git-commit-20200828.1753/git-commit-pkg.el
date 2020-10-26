6958f48ee30e0ae31e34b72280626b74b204395a
Bing Guo <changethespace@gmail.com>
2020-10-26 10:47:07 -0400

    added ace-jump, magit, packages

---
+++ elpa/git-commit-20200828.1753/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/bing/.emacs.d/elpa/git-commit-20200828.1753/git-commit.el  -*- no-byte-compile: t -*-
+(define-package "git-commit" "20200828.1753" "Edit Git commit messages" '((emacs "25.1") (dash "20200524") (transient "20200601") (with-editor "20200522")) :commit "7828691d41e0d384fdb9ddf2efd854d2b2806748" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
---
+++ elpa/magit-20201025.1624/magit-pkg.el
@@ -0,0 +1,12 @@
+(define-package "magit" "20201025.1624" "A Git porcelain inside Emacs."
+  '((emacs "25.1")
+    (async "20200113")
+    (dash "20200524")
+    (git-commit "20200516")
+    (transient "20200601")
+    (with-editor "20200522"))
+  :commit "7828691d41e0d384fdb9ddf2efd854d2b2806748" :keywords
+  '("git" "tools" "vc"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
